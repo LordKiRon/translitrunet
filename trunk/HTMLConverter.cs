62414af93b30975f496e88dd71496d91fd20596b
lordkiron <lordkiron@cc9487f2-962b-11de-99ed-9d3e36ffeeb7>
2010-07-20 07:19:08 +0000

    Added new mode "HtmlCodes" that converts text to HTML unicode format. Version advanced to 1.0.1

---
+++ trunk/HTMLConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace TranslitRu
+{
+    internal class HtmlConverter :  BaseTrasLitConverter
+    {
+        public override string ConvertCharacter(char inputCharacter)
+        {
+            StringBuilder result = new StringBuilder();
+
+            // first take care of special HTML characters like '&' or '<'
+            string str = HttpUtility.HtmlEncode(inputCharacter.ToString());
+            if (str.Length > 1)
+            {
+                return str;
+            }
+
+            int value = Convert.ToInt32(inputCharacter);
+            if (value > 127)
+            {
+                result.AppendFormat("&#{0};",value);
+            }
+            else
+            {
+                result.Append(inputCharacter);
+            }
+            return result.ToString();
+        }
+    }
+}
--- trunk/Rus2Lat.cs
+++ trunk/Rus2Lat.cs
@@ -11,6 +11,7 @@ namespace TranslitRu
         None,
         TranslitRu, // use mode used by http://www.translit.ru
         ExternalRuleFile, // use external maping file
+        HtmlCodes, // HTML codes
     }
 
     public class Rus2Lat
@@ -33,6 +34,9 @@ namespace TranslitRu
                     return converter.ConvertString(stringWithCyrillic);
                 case TranslitModeEnum.ExternalRuleFile:
                     converter = new FileBasedConverter { FileName = RuleFile };
+                    return converter.ConvertString(stringWithCyrillic);
+                case TranslitModeEnum.HtmlCodes:
+                    converter = new HtmlConverter();
                     return converter.ConvertString(stringWithCyrillic);
                 default:
                     Debug.Fail(string.Format("Invalid transliteration mode : {0}",mode));
