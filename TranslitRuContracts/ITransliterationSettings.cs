d1f068549cceecd69e4abac1c6d7f0ec6fa3a263
lordkiron@gmail.com <lordkiron@gmail.com@cc9487f2-962b-11de-99ed-9d3e36ffeeb7>
2015-03-08 13:13:14 +0000

    Added contracts

---
+++ TranslitRuContracts/ITransliterationSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Serialization;
+
+namespace TranslitRuContracts
+{
+    public enum TranslitModeEnum
+    {
+        None,
+        TranslitRu, // use mode used by http://www.translit.ru
+        ExternalRuleFile, // use external maping file
+        HtmlCodes, // HTML codes
+    }
+
+    public interface ITransliterationSettings 
+    {
+        TranslitModeEnum Mode { get; set; }
+        string FileName { get; set; }
+    }
+}
